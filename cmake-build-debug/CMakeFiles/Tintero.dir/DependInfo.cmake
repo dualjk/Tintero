e9dbdc957d2ca950e2cf86e58137d3c4051b7a70
dualjk <dnadual@gmail.com>
2020-03-07 11:41:55 +0100

    cmakefile added

---
+++ cmake-build-debug/CMakeFiles/Tintero.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/Tintero/client.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/Tintero/client.cpp.o"
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/Tintero/main.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/Tintero/main.cpp.o"
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/Tintero/mainwindow.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/Tintero/mainwindow.cpp.o"
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_client.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_client.cpp.o"
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_mainwindow.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_mainwindow.cpp.o"
+  "/Users/giuliodg/Documents/GitHub/Tintero/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/qrc_resources.cpp" "/Users/giuliodg/Documents/GitHub/Tintero/cmake-build-debug/CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/qrc_resources.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../client"
+  "../client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug"
+  "../client/Tintero"
+  "../server"
+  "../Tintero"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Tintero.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_client.cpp.o"
+  "CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/moc_mainwindow.cpp.o"
+  "CMakeFiles/Tintero.dir/client/build-Tintero-Desktop_Qt_5_14_1_clang_64bit-Debug/qrc_resources.cpp.o"
+  "CMakeFiles/Tintero.dir/client/Tintero/client.cpp.o"
+  "CMakeFiles/Tintero.dir/client/Tintero/main.cpp.o"
+  "CMakeFiles/Tintero.dir/client/Tintero/mainwindow.cpp.o"
+  "Tintero.pdb"
+  "Tintero"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tintero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
