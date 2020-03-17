7a4146810201c782e604bad4da49c2e6b8157ea3
dualjk <dnadual@gmail.com>
2020-03-17 17:15:03 +0100

    Fortune client modified
    test client-server: provato programmino stupido che manda stringhe da client a server :)

---
+++ fortune_client_modified/fortune_server_modified/Server.h
@@ -0,0 +1,41 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QDialog>
+#include <QString>
+#include <QVector>
+#include <QDataStream>
+#include <QTcpSocket>
+
+
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QTcpServer;
+class QNetworkSession;
+QT_END_NAMESPACE
+
+//! [0]
+class Server : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Server(QWidget *parent = nullptr);
+
+private slots:
+    void sessionOpened();
+    void sendFortune();
+    void receive();
+
+private:
+    QLabel *statusLabel = nullptr;
+    QTcpServer *tcpServer = nullptr;
+    QVector<QString> fortunes;
+    QNetworkSession *networkSession = nullptr;
+    QDataStream in;
+    QTcpSocket *clientConnection=nullptr;
+
+};
+//! [0]
+
+#endif
