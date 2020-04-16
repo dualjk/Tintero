fd925f9fc86c95bc24499be346a33c89fd91584c
dualjk <dnadual@gmail.com>
2020-04-16 13:33:44 +0200

    Documentazione
    Classe Uridialog
    
    Co-Authored-By: salvorusso8 <salvorusso8@users.noreply.github.com>

--- fortune_modified/fortune_client_modified/uridialog.cpp
+++ fortune_modified/fortune_client_modified/uridialog.cpp
@@ -1,3 +1,10 @@
+/*
+ * Finestra di dialogo che si apre dopo che un client ha richiesto la generazine dell'URI
+ * oppure quando il client richiede di inserire un URI. Se qualcosa dovesse andare storto
+ * la classe "page" si occupa di gestire i messaggi di errore.
+ */
+
+
 #include "uridialog.h"
 #include "ui_uridialog.h"
 
@@ -13,12 +20,18 @@ UriDialog::~UriDialog()
     delete ui;
 }
 
+
+
 void UriDialog::setUri(QString s){
     ui->uriTextEdit->setText("tintero/"+s);
     ui->uriTextEdit->setReadOnly(true);
 
 }
 
+/* La funzione si occupa di settare le label della finestra di dialogo:
+ * essendo utilizzata sia per inserire che per generare l'URI, c'è il
+ * bisogno di cambiare le label, a seconda dell'utilizzo.
+ */
 void UriDialog::setLabel(QString s){
     ui->uriLabel->setText(s);
 }
